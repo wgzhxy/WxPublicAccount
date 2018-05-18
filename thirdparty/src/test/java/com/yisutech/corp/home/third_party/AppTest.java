af863bfe17cf20b6d188c00966d430d5e107d675
东往 <wgzhxy@163.com>
2018-05-19 00:45:05 +0800

    增加模块thirdparty

---
+++ thirdparty/src/test/java/com/yisutech/corp/home/third_party/AppTest.java
@@ -0,0 +1,20 @@
+package com.yisutech.corp.home.third_party;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
