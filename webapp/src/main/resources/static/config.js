202cf127739f514bdf319c295abc1a6c2cdc8efb
东往 <wgzhxy@163.com>
2018-05-21 14:16:21 +0800

    统一配置

---
+++ webapp/src/main/resources/static/config.js
@@ -0,0 +1,7 @@
+var appConfig = {
+    appTestDomain: 'http://yisutech.s1.natapp.cc',
+    appDomain: 'http://www.yisutech.com',
+    testDomain: 'yisutech.s1.natapp.cc',
+    domain: 'www.yisutech.com'
+};
+appConfig.appDomain = appConfig.appTestDomain;
