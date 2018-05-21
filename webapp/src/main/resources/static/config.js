a47783fc2af0d0e5906770a7979c2c00c7689e3d
东往 <wgzhxy@163.com>
2018-05-21 23:22:34 +0800

    积分兑换,bugfix

--- webapp/src/main/resources/static/config.js
+++ webapp/src/main/resources/static/config.js
@@ -2,5 +2,6 @@ var appConfig = {
     appTestDomain: 'http://yisutech.s1.natapp.cc',
     appDomain: 'http://www.yisutech.com',
     testDomain: 'yisutech.s1.natapp.cc',
-    domain: 'www.yisutech.com'
+    domain: 'www.yisutech.com',
+    appId:'wxc91633dd26821ab6'
 };
