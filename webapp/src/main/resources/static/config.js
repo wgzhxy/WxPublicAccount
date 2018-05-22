8301fa011add47be6b6728dbaf49c96295e68127
东往 <wgzhxy@163.com>
2018-05-22 21:16:04 +0800

    支付页面流程增加

--- webapp/src/main/resources/static/config.js
+++ webapp/src/main/resources/static/config.js
@@ -3,5 +3,6 @@ var appConfig = {
     appDomain: 'http://www.yisutech.com',
     testDomain: 'yisutech.s1.natapp.cc',
     domain: 'www.yisutech.com',
-    appId:'wxc91633dd26821ab6'
+    appId:'wxc91633dd26821ab6',
+    testAppId:'wxc4d8005e75ad417f'
 };
