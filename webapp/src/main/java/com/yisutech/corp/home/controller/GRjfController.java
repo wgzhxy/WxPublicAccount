11ada9ced8256645c3d23901f4e9f9db71500ebb
东往 <wgzhxy@163.com>
2018-07-31 18:44:40 +0800

    积分兑换，页面，流程改造

--- webapp/src/main/java/com/yisutech/corp/home/controller/GRjfController.java
+++ webapp/src/main/java/com/yisutech/corp/home/controller/GRjfController.java
@@ -1,13 +1,16 @@
 package com.yisutech.corp.home.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.yisutech.corp.domain.repository.pojo.WxExchangeProduct;
 import com.yisutech.corp.home.service.jfmall.JfMallSrv;
+import com.yisutech.corp.home.service.user.UserSrv;
 import com.yisutech.corp.home.tools.result.Result;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
@@ -27,6 +30,8 @@ public class GRjfController {
 
 	@Resource
 	private JfMallSrv jfMallSrv;
+	@Resource
+	private UserSrv userSrv;
 
 	@RequestMapping("/jfHome")
 	public ModelAndView jfHome(Model model) {
@@ -53,31 +58,41 @@ public class GRjfController {
 	}
 
 	@RequestMapping("/jfExchange")
-	public ModelAndView jfExchange(Model model, String code, String state, String prodId) {
+	public ModelAndView jfExchange(@RequestParam String state, @RequestParam String code) {
+
+		Integer id = 0;
+		if (StringUtils.isBlank(state)) {
+			id = Integer.parseInt(state.split("@")[1]);
+		}
 
 		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("p", jfMallSrv.getExchangeProduct(id));
+		modelAndView.addObject("user", userSrv.getUserInfo(code));
+
 		modelAndView.setViewName("/jfmall/jfExchange");
+		return modelAndView;
+	}
+
+	@RequestMapping("/exchange")
+	@ResponseBody
+	public Result<Boolean> exchange(@RequestParam String code, @RequestParam String state, @RequestParam String prodId) {
 
 		// 参数检查
 		if (StringUtils.isBlank(code)) {
-			model.addAttribute("message", "<span style='color:red'>用户凭证失效，请重试</span>");
-			return modelAndView;
+			return new Result<>(false, "code_expire", "用户凭证失效，请重试");
 		}
 		if (StringUtils.isBlank(state)) {
-			model.addAttribute("message", "<span style='color:red'>兑换商品不存在</span>");
-			return modelAndView;
+			return new Result<>(false, "product_not_exists", "兑换商品不存在");
 		}
 
 		// 积分况换商品
 		prodId = state.split("@")[1];
 		Result<Boolean> result = jfMallSrv.exchange(code, state, Long.parseLong(prodId));
 		if (result.isSuccess()) {
-			model.addAttribute("message", "恭喜您，积分兑换商品成功！");
+			return new Result<>(true, "", "恭喜您，积分兑换商品成功！");
 
 		} else {
-			model.addAttribute("message", "<span style='color:red'>抱歉，积分兑换商品失败, " + result.getMsgInfo() + "</span>");
+			return new Result<>(false, "", "抱歉，积分兑换商品失败");
 		}
-
-		return modelAndView;
 	}
 }
