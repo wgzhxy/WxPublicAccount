b53ddafa824ccd83d29dcc405452b790f6d515c4
东往 <wgzhxy@163.com>
2018-08-01 12:13:43 +0800

    更新前端配置

--- webapp/src/main/resources/static/config.js
+++ webapp/src/main/resources/static/config.js
@@ -1,8 +1,21 @@
 var appConfig = {
-    appTestDomain: 'http://yisutech.s1.natapp.cc',
+    /*
+    appTestDomain: 'http://localyisu.s1.natapp.cc',
+    testDomain: 'localyisu.s1.natapp.cc',
     appDomain: 'http://www.yisutech.com',
-    testDomain: 'yisutech.s1.natapp.cc',
     domain: 'www.yisutech.com',
     appId:'wxc91633dd26821ab6',
     testAppId:'wxc4d8005e75ad417f'
+    */
+    // 测试环境
+    appDomain: 'http://localyisu.s1.natapp.cc',
+    domain: 'localyisu.s1.natapp.cc',
+    appId: 'wxc4d8005e75ad417f',
+
+    // 正式环境
+    /**
+     appDomain: 'http://www.yisutech.com',
+     domain: 'www.yisutech.com',
+     appId:'wxc91633dd26821ab6',
+     **/
 };
